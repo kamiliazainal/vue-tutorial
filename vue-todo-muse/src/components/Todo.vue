2eb5b03d60fb4a7a0aa521aac51a1f8ed73b7e1b
unknown <kamiliazainal@gmail.com>
2019-10-08 12:05:54 +0800

    todo list

---
+++ vue-todo-muse/src/components/Todo.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <!-- Muse-ui card component -->
+    <mu-card raised>
+      <mu-flex class="to-do-wrapper" justify-content="start">
+          <!-- Muse-ui checkbox component -->
+        <mu-flex
+          class="to-do-checkbox"
+          justify-content="start"
+          align-self="center"
+        >
+          <mu-checkbox v-model="isDone" value="done"></mu-checkbox>
+        </mu-flex>
+        <mu-flex
+          class="to-do-title"
+          justify-content="start"
+          align-self="center"
+        >
+          <span>{{ title }}</span>
+        </mu-flex>
+      </mu-flex>
+    </mu-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "todo",
+  props: {
+    title: String
+  },
+  // Data for checkbox
+  data() {
+      return {
+          isDone: []
+      }
+  }
+};
+</script>
+
+<style scoped>
+.mu-card {
+  margin: 20px;
+  height: 50px;
+}
+
+.to-do-wrapper {
+  width: 100%;
+  height: 50px;
+}
+
+.to-do-checkbox {
+  margin: 0px 20px;
+}
+
+.to-do-title {
+  margin: 0px 20px;
+}
+</style>
